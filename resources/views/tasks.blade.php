29c8e96921803889f1fcff67c7ea8ad65844aa34
admin <admin@dewan.gov.ps>
2025-02-20 19:55:27 +0200

    first commit

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -32,6 +32,30 @@
     <div class="container mt-4">
         <div class="offset-md-2 col-md-8">
             <div class="card">
+                @if (isset($task))
+                <div class="card-header">
+                    Update Task
+                </div>
+                <div class="card-body">
+                    <!-- Update Task Form -->
+                    <form action="{{url('update')}}" method="POST">
+                        @csrf
+                        <input type="hidden" name="id" value="{{$task->id}}">
+                        <!-- Task Name -->
+                        <div class="mb-3">
+                            <label for="task-name" class="form-label">Task</label>
+                            <input type="text" name="name" id="task-name" class="form-control" value="{{$task->name}}">
+                        </div>
+                        <!-- Update Task Button -->
+                        <div>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fa fa-plus me-2"></i>Update Task
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+                @else
                 <div class="card-header">
                     New Task
                 </div>
@@ -53,6 +77,7 @@
                         </div>
                     </form>
                 </div>
+                @endif
             </div>
 
             <!-- Current Tasks -->
@@ -69,36 +94,26 @@
                             </tr>
                         </thead>
                         <tbody>
+                            @foreach ($tasks as $task)
                             <tr>
-                                <td>Task 1</td>
+                                <td>{{$task->name}}</td>
                                 <td>
-                                    <form action="#" method="POST" class="d-inline">
+                                    <form action="/delete/{{$task->id}}" method="POST" class="d-inline">
+                                        @csrf
                                         <button type="submit" class="btn btn-danger">
                                             <i class="fa fa-trash me-2"></i>Delete
                                         </button>
                                     </form>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Task 2</td>
-                                <td>
-                                    <form action="#" method="POST" class="d-inline">
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash me-2"></i>Delete
+                                    <form action="/edit/{{$task->id}}" method="POST" class="d-inline">
+                                        @csrf
+                                        <button type="submit" class="btn btn-info">
+                                            <i class="fa fa-info me-2"></i>Edit
                                         </button>
                                     </form>
                                 </td>
                             </tr>
-                            <tr>
-                                <td>Task 3</td>
-                                <td>
-                                    <form action="#" method="POST" class="d-inline">
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash me-2"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
+                            @endforeach
+                            
                         </tbody>
                     </table>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,32 @@ Route::post(uri: '/about', action: function() {
 
 
 Route::get(uri: 'tasks', action: function() {
-    return view(view: 'tasks');
+    $tasks = DB::table(table: 'tasks')->get();
+
+    return view(view: 'tasks', data: compact('tasks'));
 });
 
 Route::post(uri: 'create', action: function(): string{
     $task_name = $_POST['name'];
     DB::table(table: 'tasks')->insert(values: ['name' => $task_name]);
-    return view(view: 'tasks') ;
+    return redirect()->back();
+});
+
+Route::post(uri: 'delete/{id}', action: function($id) {
+    DB::table(table: 'tasks')->where(column: 'id', operator: $id)->delete();
+    return redirect()->back();
+});
+
+Route::post(uri: 'edit/{id}', action: function($id) {
+    $task = DB::table(table: 'tasks')->where(column: 'id', operator: $id)->first();
+    $tasks = DB::table(table: 'tasks')->get();
+    return view(view: 'tasks', data: compact('task', 'tasks'));
+
+});
+
+Route::post(uri: 'update', action: function() {
+    $id = $_POST['id'];
+    DB::table(table: 'tasks')->where('id','=', $id)->update(['name' => $_POST['name']]);
+    return redirect(to: 'tasks');
+
 });
